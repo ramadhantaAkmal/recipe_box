c3549a948fa497c77da2c2831b05f7e21479f8f7
M Haris Wicaksono <hariswicaksono250999@gmail.com>
2023-03-29 14:33:55 +0700

    merge

--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -3,7 +3,7 @@ const { RecipeController } = require("../controllers");
 
 recipeRoute.get("/", RecipeController.listMyRecipe);
 recipeRoute.post("/", RecipeController.addRecipes);
-recipeRoute.get("/:id", RecipeController.getRecipeByID);
+recipeRoute.get("/:id", RecipeController.getRecipeDetails);
 recipeRoute.get("/delete/:id", RecipeController.deleteRecipe);
 recipeRoute.post("/update/:id", RecipeController.updateRecipe);
 
