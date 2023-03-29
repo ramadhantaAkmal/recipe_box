17e894b4a4d203d0e74a5d9c4e2b3e9c90321718
Mohamad Akmal Ramadhanta <42136962+ramadhantaAkmal@users.noreply.github.com>
2023-03-29 22:20:54 +0700

    fix

--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -5,7 +5,7 @@ recipeRoute.get("/", RecipeController.listMyRecipe);
 recipeRoute.post("/", RecipeController.addRecipes);
 recipeRoute.get("/:id", RecipeController.getRecipeDetails);
 recipeRoute.get("/add", RecipeController.showAddRecipes)
-// recipeRoute.get("/:id", RecipeController.getRecipeByID);
+recipeRoute.get("/:id", RecipeController.getRecipeByID);
 recipeRoute.get("/delete/:id", RecipeController.deleteRecipe);
 recipeRoute.post("/update/:id", RecipeController.updateRecipe);
 recipeRoute.get("/update/:id", RecipeController.updateRecipePage);
