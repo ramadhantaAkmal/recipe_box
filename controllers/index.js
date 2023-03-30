729b748515520ed8c408fb7cb30447ea3ea73659
Mohamad Akmal Ramadhanta <42136962+ramadhantaAkmal@users.noreply.github.com>
2023-03-31 04:53:22 +0700

    feat: category-seeder

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,9 @@
 const UserController = require("./UserController");
 const AuthController = require("./AuthController");
 const RecipeController = require("./RecipeController");
-const IngredientController = require("./IngredientController");
-const RecipeCategoryController = require("./RecipeCategoryController");
 
 module.exports = {
   UserController,
   AuthController,
   RecipeController,
-  IngredientController,
-  RecipeCategoryController,
 };
